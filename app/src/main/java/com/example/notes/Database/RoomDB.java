3fdce4cad782f0f53d4cf867684caaa68a2e218e
Aseempaul007 <aseempal007@gmail.com>
2023-03-22 23:26:10 +0530

    RoomDB.java class added!

---
+++ app/src/main/java/com/example/notes/Database/RoomDB.java
@@ -0,0 +1,33 @@
+package com.example.notes.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.notes.Models.Notes;
+
+@Database(entities = Notes.class, exportSchema = false,version = 1)
+public abstract class RoomDB extends RoomDatabase {
+
+    private static RoomDB database;
+    private static String DATABASE_NAME = "NotesApp";
+
+    public static synchronized RoomDB getInstance(Context context){
+        if(database==null){
+            database = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    RoomDB.class,
+                    DATABASE_NAME
+            )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return database;
+    }
+
+    public abstract MainDao mainDao();
+
+}
